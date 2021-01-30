1900a99d7bad90bbaa6989a6ea6bcd694042c724
Bert <a3n3a_1337@protonmail.com>
2021-01-30 17:38:55 +0100

    modularisation of functions + bokeh import
    created python file which will contain functions that work with the CVE json datafeed, that have clear defined goal
    import bokeh for data visualisation

---
+++ dataFunctions.py
@@ -0,0 +1,28 @@
+def getCVEIDs(cveJsonFile):
+    cveIDs = []
+    for cve in cveJsonFile['CVE_Items']:
+        # print(cve['cve']['CVE_data_meta']['ID'])
+        cveIDs.append(cve['cve']['CVE_data_meta']['ID'])
+    return cveIDs
+
+
+def getCVEbyYear(CVEIDs):
+    cve1999 = 0
+    cve2000 = 0
+    cve2001 = 0
+    cve2002 = 0
+    rest = 0
+    for cveID in cveIDs:
+        if 'CVE-1999' in cveID:
+            cve1999 += 1
+        elif 'CVE-2000' in cveID:
+            cve2000 += 1
+        elif 'CVE-2001' in cveID:
+            cve2001 += 1
+        elif 'CVE-2002' in cveID:
+            cve2002 += 1
+        else:
+            rest += 1
+            print(cveID)
+    if rest == 0: #todo: c
+        return (list(cve1999,cve2000,cve2001,cve2002))
--- main.py
+++ main.py
@@ -1,22 +1,36 @@
 import json
+import dataFunctions as DF
+import bokeh.s
 
 cve2002File = open('datafeeds/nvdcve-1.1-2002.json','r')
-
 cveJson = json.loads(cve2002File.read())
 
-cveIDs = []
 
-for cve in cveJson['CVE_Items']:
-    #print(cve['cve']['CVE_data_meta']['ID'])
-    cveIDs.append(cve['cve']['CVE_data_meta']['ID'])
-0
-print(len(cveIDs))
+# testing
+# for cve in cveJson['CVE_Items']:
+#     print(cve['cve']['CVE_data_meta']['ID'])
+    
+
+# TOTAL COUNT CVE
+cveIDs = DF.getCVEIDs(cveJson)
+ct_CVEs = len(cveIDs)
+print(ct_CVEs)
+
+
+CVEsByYear = DF.getCVEbyYear(cveIDs)
+years = ['1999','2000','2001','2002']
+
+
+
+
+
+
+
+
+
+
+
+
 
 cve2002File.close()
 
-def getCVEIDs(jsonFile):
-    cveIDs = []
-    for cve in cveJson['CVE_Items']:
-        #print(cve['cve']['CVE_data_meta']['ID'])
-        cveIDs.append(cve['cve']['CVE_data_meta']['ID'])
-    return cveIDs
